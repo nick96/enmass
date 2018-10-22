eabb38ad87dba838c63c1fc8ce76fe981174378d
Nick Spain <nicholas.spain96@gmail.com>
2018-10-22 23:47:56 +1100

    Restructure to fit recommended guidlines

--- messaging/interfaces.go
+++
@@ -1,9 +0,0 @@
-package messaging
-
-//go:generate mockgen -destination=../mocks/messaging_mocks.go -package mocks github.com/nick96/enmass/messaging Client
-
-// Client represents the contract that must be implemented for a class
-// to qualify as a client.
-type Client interface {
-	SendMessage(from string, to string, msg string) error
-}
