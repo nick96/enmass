964756c3c50cb9f6dc5aa6b9a978fd5d27dc9d1a
Nick Spain <nicholas.spain96@gmail.com>
2019-06-15 23:38:50 +1000

    Start writing witheditor module

---
+++ witheditor/witheditor.go
@@ -0,0 +1,6 @@
+package witheditor
+
+// WithEditor opens EDITOR with signature SIGNATURE and comment COMMENT
+func WithEditor(editor string, signature string, comment string) (string, error) {
+	return "", nil
+}
